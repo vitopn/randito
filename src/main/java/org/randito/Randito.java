4f4d10999def7b07b23f6562d86fa5fe2571a8a2
Vito Paine <vito.paine@presidioHealth.com>
2016-01-30 19:00:13 -0700

    random_string

---
+++ src/main/java/org/randito/Randito.java
@@ -0,0 +1,15 @@
+package org.randito;
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+@Target(FIELD)
+@Retention(RUNTIME)
+@Documented
+@interface Randito {
+
+}
